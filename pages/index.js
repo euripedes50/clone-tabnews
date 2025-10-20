c589cc7fa41329adecf91ffaab58c3aed1800d2c
Eurípedes Pereira <euripedespereira00@gmail.com>
2025-10-20 18:10:14 -0300

    adicionado scripts `lint:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-  return <h1>#Em contrução# Versão de estudo do TabNews: igual, mas feita com suor e café!</h1>
+  return (
+    <h1>
+      #Em contrução# Versão de estudo do TabNews: igual, mas feita com suor e
+      café!
+    </h1>
+  );
 }
 
 export default Home;
-
